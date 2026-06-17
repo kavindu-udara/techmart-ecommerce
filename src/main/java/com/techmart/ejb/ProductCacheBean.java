320b028feb1ece7589e4857574e28c434ba9ee1f
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-17 09:52:26 +0530

    created ProductCacheBean

---
+++ src/main/java/com/techmart/ejb/ProductCacheBean.java
@@ -0,0 +1,53 @@
+package com.techmart.ejb;
+
+import com.techmart.entity.Product;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Singleton
+@Startup
+@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
+public class ProductCacheBean {
+
+    private static final Logger logger = Logger.getLogger(ProductCacheBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //    In memo cache
+    private List<Product> productCache;
+
+    @PostConstruct
+    public void init() {
+        logger.info("Initializing ProductCacheBean");
+        loadCache();
+        logger.info("ProductCacheBean initialized successfully with " + productCache.size() + " products.");
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        logger.info("Destroying Product Cache and freeing memory...");
+        productCache = null;
+    }
+
+    @Lock(LockType.READ)
+    public Product getProductById(Long id) {
+        return productCache.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Lock(LockType.WRITE)
+    public void refreshCache() {
+        logger.info("Refreshing Product Cache...");
+        loadCache();
+    }
+
+    private void loadCache() {
+        this.productCache = em.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+    }
+}
