0eb7093a7215e8a652dcf52a0a2adb07ecc8eb70
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-21 16:03:59 +0530

    created email validator

--- src/main/java/com/techmart/util/Validators.java
+++ src/main/java/com/techmart/util/Validators.java
@@ -1,4 +1,12 @@
 package com.techmart.util;
 
 public class Validators {
+
+    public static boolean isValidEmail(String email){
+        if(email == null){
+            return false;
+        }
+        return Values.EMAIL_PATTERN.matcher(email).matches();
+    }
+
 }
