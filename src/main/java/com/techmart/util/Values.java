27ca3aca87d0009753bcfdf2d55711a1ba6ae276
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-21 15:49:26 +0530

    created values class for store constants

---
+++ src/main/java/com/techmart/util/Values.java
@@ -0,0 +1,9 @@
+package com.techmart.util;
+
+import java.util.regex.Pattern;
+
+public class Values {
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+}
