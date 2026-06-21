0bbaa30927240840018e3bcf45e6119e0cb7bd1b
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-21 16:04:16 +0530

    added email regex patterns

--- src/main/java/com/techmart/util/Values.java
+++ src/main/java/com/techmart/util/Values.java
@@ -4,6 +4,6 @@ import java.util.regex.Pattern;
 
 public class Values {
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    public static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
 }
