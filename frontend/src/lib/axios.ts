f4fc2746435500acd62829188f0e82ea4f0ce325
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-22 16:57:35 +0530

    updated axios to send auth token everytime

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,14 @@
 import axios from "axios";
 
+// get the token from localStorage and set it in the headers of every request
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
