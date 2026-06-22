0a77bed00ee59cca303098e9a3ac48a6e76dd6fb
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-22 11:21:33 +0530

    removed username field

--- src/main/java/com/techmart/entity/User.java
+++ src/main/java/com/techmart/entity/User.java
@@ -14,9 +14,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
-    private String username;
-
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
@@ -37,14 +34,6 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getEmail() {
