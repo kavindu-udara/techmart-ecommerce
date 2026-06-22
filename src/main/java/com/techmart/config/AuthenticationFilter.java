c3a01cd8725a2f55b66a6df9873772927202fb05
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-22 15:40:25 +0530

    created auth middleware class

---
+++ src/main/java/com/techmart/config/AuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.techmart.config;
+
+import com.techmart.util.JwtUtil;
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Secured
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+//        get the Authorization header
+        String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+//        check if header exist and start with "Bearer "
+        if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
+            abortWithUnauthorized(requestContext, "Missing or invalid Authorization header");
+            return;
+        }
+
+//        extract the token
+        String token = authHeader.substring(BEARER_PREFIX.length()).trim();
+
+        try {
+            String userIdString = JwtUtil.validateTokenAndGetUserId(token);
+
+//            store user id in the request context
+            requestContext.setProperty("authenticatedUserId", Long.parseLong(userIdString));
+        } catch (Exception e) {
+            abortWithUnauthorized(requestContext, "Invalid or expired token.");
+        }
+
+    }
+
+    private void abortWithUnauthorized(ContainerRequestContext requestContext, String message) {
+        requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED)
+                .entity("{\"error\": \"" + message + "\"}")
+                .build());
+    }
+}
