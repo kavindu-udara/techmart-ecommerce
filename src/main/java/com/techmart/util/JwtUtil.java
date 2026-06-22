a7e973f0126f81dd9032cccf44c84a28e83a0e85
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-22 15:38:20 +0530

    update env example to add jwt token and update values to get the token from env vars

--- src/main/java/com/techmart/util/JwtUtil.java
+++ src/main/java/com/techmart/util/JwtUtil.java
@@ -1,4 +1,31 @@
 package com.techmart.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 public class JwtUtil {
+
+    public static String generateToken(Long userId, String email) {
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .claim("email", email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + Values.JWT_EXPIRATION_TIME))
+                .signWith(Values.JWT_KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static String validateTokenAndGetUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Values.JWT_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject(); // Returns the userId as a String
+    }
+
 }
--- src/main/java/com/techmart/util/Values.java
+++ src/main/java/com/techmart/util/Values.java
@@ -1,9 +1,16 @@
 package com.techmart.util;
 
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
 import java.util.regex.Pattern;
 
 public class Values {
     private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     public static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    private static final String JWT_SECRET_KEY = System.getenv("JWT_SECRET_KEY");
+    public static final Key JWT_KEY = Keys.hmacShaKeyFor(JWT_SECRET_KEY.getBytes());
+    public static final long JWT_EXPIRATION_TIME = 864_000_000; // 24 hrs
 
 }
