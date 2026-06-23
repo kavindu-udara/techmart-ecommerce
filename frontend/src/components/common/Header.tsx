7aa3d0e5513550bab04a3b58a857e784bc951961
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-23 06:41:10 +0530

    handle logout

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,10 +1,16 @@
 import { Button } from "../ui/button"
 
 const Header = () => {
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+    }
+
   return (
     <header className="w-full border-b flex flex-row justify-between items-center p-3 shadow-sm">
       <h1>Header</h1>
-      <Button>Logout</Button>
+      <Button onClick={handleLogout}>Logout</Button>
     </header>
   )
 }
