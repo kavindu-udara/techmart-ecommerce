2a29f7832e89b2d8d20ecc67bcfa0c71525dfc34
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-24 17:49:47 +0530

    created cart item update dialog

--- frontend/src/components/sections/CartSection.tsx
+++ frontend/src/components/sections/CartSection.tsx
@@ -1,19 +1,31 @@
+import { useRef, useState } from "react";
 import type { Cart } from "../../types/cart"
 import CartItemCard from "../cards/CartItemCard";
 import { Button } from "../ui/button";
+import UpdateCartItemDialog from "../dialogs/UpdateCartItemDialog";
 
 type Props = {
     cart: Cart;
 }
 
 const CartSection = ({ cart }: Props) => {
+
+    const dialogBtnRef = useRef<HTMLButtonElement>(null);
+    
+    const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+    
+    const handleUpdateCartItem = (itemId: number) => {
+        setSelectedItemId(itemId);
+        dialogBtnRef.current?.click();
+    }
+
     return (
         <div className="container mx-auto p-4">
             {
                 cart.items.length > 0 ? (
                     <div className="w-full flex flex-col gap-4">
                         {cart.items.map(item => (
-                            <CartItemCard key={item.itemId} item={item} />
+                            <CartItemCard key={item.itemId} item={item} handleUpdate={handleUpdateCartItem} />
                         ))}
                     </div>
                 ) : (
@@ -27,6 +39,8 @@ const CartSection = ({ cart }: Props) => {
                 <h2 className="text-xl font-bold">Total: ${cart.totalAmount.toFixed(2)}</h2>
                 <Button>Place Order</Button>
             </div>
+
+            <UpdateCartItemDialog triggerRef={dialogBtnRef} itemId={selectedItemId} />
 
         </div>
     )
