fd370f3d0d8e2590777d4bf7132cdb37f54d9134
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-24 14:27:41 +0530

    product section created

---
+++ frontend/src/components/sections/SingleProductSection.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+import type { Product } from "../../types/product"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+
+type Props = {
+    product: Product
+}
+
+const SingleProductSection = ({ product }: Props) => {
+
+    const [quantity, setQuantity] = useState<number>(1);
+
+    const handleQuantityChange = (type: "increment" | "decrement") => {
+        if (type === "increment") {
+            setQuantity(Math.min(product.stockQuantity, quantity + 1));
+        } else {
+            setQuantity(Math.max(1, quantity - 1));
+        }
+    };
+
+    return (
+        <div className="container mx-auto p-4 grid grid-cols-2 gap-8">
+            <div>
+                <img src={product.imageUrl} alt={product.name} className="w-full h-auto object-cover" />
+            </div>
+            <div className="flex flex-col justify-center gap-4">
+                <h1>{product.name}</h1>
+                <p>Price: ${product.price.toFixed(2)}</p>
+                <p>Stock: {product.stockQuantity}</p>
+                <div className="flex flex-row gap-2">
+                    <Button variant={"secondary"} onClick={() => handleQuantityChange("decrement")}>
+                        -
+                    </Button>
+                    <Input type="number" value={quantity} onChange={(e) => {
+                        const value = parseInt(e.target.value);
+                        if (!isNaN(value) && value >= 1 && value <= product.stockQuantity) {
+                            setQuantity(value);
+                        }
+                    }} className="w-16 text-center" min={1} max={product.stockQuantity} />
+                    <Button variant={"secondary"} onClick={() => handleQuantityChange("increment")}>
+                        +
+                    </Button>
+                </div>
+                <Button onClick={() => alert("Add to cart functionality not implemented yet")}>Add to Cart</Button>
+            </div>
+        </div>
+    )
+}
+
+export default SingleProductSection
--- frontend/src/pages/SingleProductPage.tsx
+++ frontend/src/pages/SingleProductPage.tsx
@@ -1,14 +1,41 @@
 import { useParams } from "react-router";
 import AuthLayout from "../components/layouts/AuthLayout";
+import { useEffect, useState } from "react";
+import { apiClient } from "../lib/axios";
+import type { Product } from "../types/product";
+import SingleProductSection from "../components/sections/SingleProductSection";
 
 const SingleProductPage = () => {
     const { id } = useParams();
 
-  return (
-    <AuthLayout>
-      single product page for product id: {id}
-    </AuthLayout>
-  )
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        apiClient.get(`/products/${id}`)
+            .then(response => {
+                setProduct(response.data);
+                console.log("Fetched product:", response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching product:", error);
+            });
+    }, []);
+
+    return (
+        <AuthLayout>
+            <main className="w-full flex flex-col items-center  min-h-screen">
+                {
+                    product ? (
+                        <SingleProductSection product={product} />
+                    ) : (
+                        <div className="w-full min-h-screen flex items-center justify-center">
+                            Loading....
+                        </div>
+                    )
+                }
+            </main>
+        </AuthLayout>
+    )
 }
 
 export default SingleProductPage
