b8a21ed99797335498430276f9b3d42e4559266b
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-25 08:23:35 +0530

    order section created

---
+++ frontend/src/components/sections/OrdersSection.tsx
@@ -0,0 +1,19 @@
+import type { Order } from "../../types/order"
+import OrderItemCard from "../cards/OrderItemCard"
+
+type Props = {
+    orders: Order[]
+}
+
+const OrdersSection = ({ orders }: Props) => {
+    return (
+        <div className="relative w-full flex flex-col gap-4">
+            {orders.map((order: Order) => (
+                <OrderItemCard key={order.id} order={order} />
+            ))}
+          
+        </div>
+    )
+}
+
+export default OrdersSection
