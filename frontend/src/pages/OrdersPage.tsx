48efdf1436a6c836df8dd755a988f6c78625ecc7
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-25 08:23:45 +0530

    order page completed

--- frontend/src/pages/OrdersPage.tsx
+++ frontend/src/pages/OrdersPage.tsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react"
 import AuthLayout from "../components/layouts/AuthLayout"
+import { apiClient } from "../lib/axios";
+import type { Order } from "../types/order";
+import OrdersSection from "../components/sections/OrdersSection";
 
 const OrdersPage = () => {
-  return (
-    <AuthLayout>
-      <main>
-        orders page
-      </main>
-    </AuthLayout>
-  )
+
+    const [orders, setOrders] = useState<Order[]>([]);
+
+    useEffect(() => {
+        apiClient.get('/orders').then((response) => {
+            console.log("Fetched orders:", response.data);
+            setOrders(response.data);
+        })
+            .catch((error) => {
+                console.error("Failed to fetch orders:", error);
+                alert("Failed to fetch orders");
+            });
+    }, []);
+
+    return (
+        <AuthLayout>
+            <main className="w-full flex justify-center">
+                <div className="container mx-auto p-4">
+                    <h1 className="text-2xl font-bold mb-4">Your Orders</h1>
+                    {
+                        orders.length > 0 ? (
+                            <OrdersSection orders={orders} />
+                        ) : (
+                            <div className="w-full min-h-screen flex items-center justify-center">
+                                You have no orders.
+                            </div>
+                        )
+                    }
+                </div>
+            </main>
+        </AuthLayout>
+    )
 }
 
 export default OrdersPage
