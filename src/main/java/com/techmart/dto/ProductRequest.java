974ea66b0a5873388588384c22e2c497777f2f52
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-27 00:06:38 +0530

    created ProductRequest dto

---
+++ src/main/java/com/techmart/dto/ProductRequest.java
@@ -0,0 +1,53 @@
+package com.techmart.dto;
+
+import java.math.BigDecimal;
+
+public class ProductRequest {
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private String imageUrl;
+
+    public ProductRequest() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
