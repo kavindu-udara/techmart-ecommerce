25dbd70f9b1813f352cf61d4c749af2a1f0c006c
kavindu-udara <karunasinghesampath38@gmail.com>
2026-06-27 22:00:05 +0530

    admin dashboard page updated

--- frontend/src/pages/admin/AdminDashboardPage.tsx
+++ frontend/src/pages/admin/AdminDashboardPage.tsx
@@ -1,22 +1,42 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import AuthLayout from "../../components/layouts/AuthLayout"
 import { apiClient } from "../../lib/axios";
+import type { AdminDashboardResponseType } from "../../types/admin";
+import DashboardStatsSection from "../../components/sections/DashboardStatsSection";
 
 const AdminDashboardPage = () => {
 
+    const [isLoading, setIsLoading] = useState(true);
+    const [dashboardData, setDashboardData] = useState<AdminDashboardResponseType | null>(null);
+
     useEffect(() => {
+        setIsLoading(true);
         apiClient.get("/admin/dashboard").then((response) => {
-            console.log("Admin dashboard data:", response.data);
+            setDashboardData(response.data);
         }).catch((error) => {
             console.error("Error fetching admin dashboard data:", error);
+        }).finally(() => {
+            setIsLoading(false);
         });
     }, []);
 
     return (
         <AuthLayout>
-            <main>
-                admin dashboard
-            </main>
+            {
+                isLoading ? (
+                    <div className="w-full min-h-screen flex items-center justify-center">
+                        Loading....
+                    </div>
+                ) : (
+                    <>
+                        {dashboardData ? (
+                            <DashboardStatsSection stats={dashboardData} />
+                        ) : (
+                            <div className="text-red-500">Failed to load dashboard data.</div>
+                        )}
+                    </>
+                )
+            }
         </AuthLayout>
     )
 }
